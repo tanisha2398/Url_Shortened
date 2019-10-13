242aa96a2ef0536eb203af8c5e8335f73966536f
tanisha2398 <tanishanegipro@gmail.com>
2019-10-13 18:10:52 +0530

    add consistency for url

--- client/src/container/InputUrl.js
+++ client/src/container/InputUrl.js
@@ -42,12 +42,16 @@ class InputUrl extends Component {
       .catch(err => {
         this.setState({
           error: true,
-          loading: false
+          loading: false,
+          shortUrl: `https://tinysa.herokuapp.com/${res.data}`,
+          value: ""
         });
       });
   }
   render() {
-    let errorText = this.state.error ? <h1>something went wrong 😭</h1> : null;
+    let errorText = this.state.error ? (
+      <h1>Already shortened with url 😭</h1>
+    ) : null;
     let copyText = this.state.copied ? <h1>Copied &#128077;</h1> : null;
     let link =
       this.state.shortUrl !== "" ? (
